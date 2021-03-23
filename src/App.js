80768fff71fab9362ccff2ea2ac7c6651349207d
Avilash2001 <avilashghosh2001@gmail.com>
2021-03-23 15:50:06 +0530

    frontend ui

--- src/App.js
+++ src/App.js
@@ -1,75 +1,125 @@
-import { useEffect } from 'react';
-import './App.css';
-import { getApiData } from "./dataApi";
+import { useEffect, useState } from "react";
+import "./App.css";
+// import { getApiData } from "./dataApi";
+import axios from "axios";
 
 function App() {
-  const apiData = getApiData();
-  const coords=[]
-  const sats =[]
+  const [ip, setIp] = useState("");
+  const [port, setPort] = useState("");
+  const [showMap, setShowMap] = useState(false);
+  const apiData = [];
+  const coords = [];
+  const sats = [];
+  const time = 1200;
+  useEffect(() => {
+    const interval = setInterval(() => {
+      axios
+        // .get("http://192.168.29.49:3000/gps/relive/updated.json")
+        .get("http://192.168.29.49:3000/gps/live")
+        .then((res) => {
+          apiData.push(res.data.GPS);
+          for (let i = 0; i < apiData.length; i++) {
+            if (
+              parseFloat(apiData[i].longi) !== 0 &&
+              parseFloat(apiData[i].lati) !== 0
+            ) {
+              const temp = [];
+              sats.push(apiData[i].sats);
+              temp.push(parseFloat(apiData[i].longi));
+              temp.push(parseFloat(apiData[i].lati));
+              coords.push(temp);
+            } else {
+              console.log("hello");
+            }
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
 
+      console.log(apiData);
+    }, time);
 
-for (let i =0 ; i<apiData.length;i++){
-  if(parseFloat(apiData[i].longi) !== 0 && parseFloat(apiData[i].lati) !== 0)  
-  {
-    const temp = []
-    sats.push(apiData[i].sats)
-    temp.push(parseFloat(apiData[i].longi))
-    temp.push(parseFloat(apiData[i].lati))
-    coords.push(temp)
-  }
-  else{
-    console.log("hello")
-  }
-}
+    return () => clearInterval(interval);
+  }, [time, apiData]);
 
-// console.log(coords)
-// console.log(sats)
+  const mapRefesh = 5000;
 
-useEffect(() => {
-  var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
-  mapboxgl.accessToken = 'pk.eyJ1IjoiYXZpeGlvbjIwMDEiLCJhIjoiY2ttN2p1MjY4MHl2YjJ5bzQxOXNkamJzeSJ9.gY48YPfe2qT9V_6zF6Tj-w';
-const map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/streets-v11',
-// center: [88.42451567, 26.71762767],
-center: coords[Math.floor(apiData.length/2)],
-zoom: 15
-});
-map.addControl(new mapboxgl.NavigationControl());
+  useEffect(() => {
+    const interval = setInterval(() => {
+      var mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
+      mapboxgl.accessToken =
+        "pk.eyJ1IjoiYXZpeGlvbjIwMDEiLCJhIjoiY2ttN2p1MjY4MHl2YjJ5bzQxOXNkamJzeSJ9.gY48YPfe2qT9V_6zF6Tj-w";
+      const map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: coords[Math.floor(apiData.length / 2)],
+        zoom: 13,
+      });
+      map.addControl(new mapboxgl.NavigationControl());
 
-map.on('load', function () {
-    map.addSource('route', {
-    'type': 'geojson',
-    'data': {
-    'type': 'Feature',
-    'properties': {},
-    'geometry': {
-    'type': 'LineString',
-    'coordinates': coords
-    }
-    }
-    });
-    map.addLayer({
-    'id': 'route',
-    'type': 'line',
-    'source': 'route',
-    'layout': {
-    'line-join': 'round',
-    'line-cap': 'round'
-    },
-    'paint': {
-    'line-color': '#cf2626',
-    'line-width': 8
-    }
-    });
-    });
-  
-}, [coords])
+      map.on("load", function () {
+        map.addSource("route", {
+          type: "geojson",
+          data: {
+            type: "Feature",
+            properties: {},
+            geometry: {
+              type: "LineString",
+              coordinates: coords,
+            },
+          },
+        });
+        map.addLayer({
+          id: "route",
+          type: "line",
+          source: "route",
+          layout: {
+            "line-join": "round",
+            "line-cap": "round",
+          },
+          paint: {
+            "line-color": "#cf2626",
+            "line-width": 8,
+          },
+        });
+      });
+    }, mapRefesh);
+
+    return () => clearInterval(interval);
+  }, [coords, mapRefesh]);
+
+  const handleClick = () => {
+    setShowMap(true);
+  };
   return (
-    <div className="App" style={{width: "100vw", height: "100vh", display: "flex", alignItems:"center", justifyContent:"center"}}>
-      <div id="map" style={{width: "100%", height: "100%"}}></div>
-
-
+    <div>
+      {/* {!showMap && (
+        <div>
+          <input type="text" placeholder="IP" value={ip} onChange={setIp} />
+          <input
+            type="text"
+            placeholder="Port"
+            value={port}
+            onChange={setPort}
+          />
+          <button onChange={handleClick}>OK</button>
+        </div>
+      )}
+      {showMap && ( */}
+      <div
+        className="App"
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div id="map" style={{ width: "100%", height: "100%" }}></div>
+      </div>
+      {/* )} */}
     </div>
   );
 }
